3cc3af2045373f34eb26dd864437982bf807ef02
marcon29 <online@markstabler.com>
2022-03-26 15:24:47 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let allTaskItems = []
+  const taskList = document.getElementById("tasks")
+  const taskItemInput = document.getElementById("new-task-description")
+
+  document.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskItem = taskItemInput.value;
+    saveTaskItem(taskItem);  
+    addItemToTaskList(taskItem);
+    clearFormInput();
+    }
+  )
+
+  function saveTaskItem(taskItem) {
+    allTaskItems.push(taskItem);
+  }
+
+  function addItemToTaskList(taskItem) {
+    const taskItemElement = document.createElement("li")
+    taskItemElement.textContent = taskItem
+    taskList.appendChild(taskItemElement)  
+  }
+
+  function clearFormInput() {
+    taskItemInput.value = ""
+  }
 });
+
+
+
